5ce430c176c0c90f6f78656c76ed01ddf3917f67
riyanfbri1 <92567991+riyanfbri1@users.noreply.github.com>
2023-04-08 01:05:10 +0700

    Initial commit LANJUTAN

---
+++ app/src/main/java/com/riyan/lanjutanwebview/WebAppInterface.java
@@ -0,0 +1,37 @@
+package com.riyan.lanjutanwebview;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.webkit.JavascriptInterface;
+import android.widget.Toast;
+
+public class WebAppInterface
+{
+    private Activity _activity;
+    private Context _context;
+
+    public WebAppInterface(Context context, Activity activity){
+        _context = context;
+        _activity = activity;
+    }
+    @JavascriptInterface
+    public void showToast(String message)
+    {
+        Toast.makeText(_context, message, Toast.LENGTH_LONG).show();
+    }
+    @JavascriptInterface
+    public void showSms()
+    {
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_APP_MESSAGING);
+        _context.startActivity(intent);
+    }
+    @JavascriptInterface
+    public void showDate()
+    {
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_APP_CALENDAR);
+        _context.startActivity(intent);
+    }
+}
